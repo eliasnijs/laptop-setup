2f14ea35e30aa09f3cc212ca6cbcaf57ba46f19e
Elias Nijs <elias.nijs@gmail.com>
2021-06-30 01:58:00 +0200

    added volume keys

--- dwm-configuration/config.def.h
+++ dwm-configuration/config.def.h
@@ -1,4 +1,5 @@
 /* See LICENSE file for copyright and license details. */
+#include <X11/XF86keysym.h>
 
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
@@ -62,9 +63,13 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray1, NULL };
 static const char *termcmd[]  = { "urxvt", NULL };
+static const char *audiovolup[] = {"audio-increment","plus","10", NULL};
+static const char *audiovoldown[] = {"audio-increment","min","10", NULL};
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
+   { 0,              XF86XK_AudioRaiseVolume, spawn,          {.v = audiovolup } },
+   { 0,              XF86XK_AudioLowerVolume, spawn,          {.v = audiovoldown } },
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
