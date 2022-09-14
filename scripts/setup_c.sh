9a6c9d669a612c999dd026734142bc6106e68819
Elias Nijs <elias.nijs@gmail.com>
2022-09-14 20:08:48 +0200

    update

---
+++ scripts/setup_c.sh
@@ -0,0 +1,11 @@
+
+mkdir build
+mkdir resources
+mkdir data
+mkdir src
+
+touch README
+touch build.sh
+touch run.sh
+
+
--- sucklesstools/dwm-6.2/config.h
+++ sucklesstools/dwm-6.2/config.h
@@ -18,7 +18,7 @@ static const char col_cyan[]        = "#40FF40";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2  },
-	[SchemeSel]  = { col_gray4, col_gray1,  col_cyan  },
+	[SchemeSel]  = { col_cyan, col_gray1,  col_cyan  },
 };
 
 /* tagging */
